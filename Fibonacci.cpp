7e6508a0164f5a2e67348b69a718cd6bb4d2ca44
Yuvraj2159 <yubrajp54@gmail.com>
2023-10-05 15:05:55 +0545

    first commit

---
+++ Fibonacci.cpp
@@ -0,0 +1,18 @@
+//IMPORTANT//
+//WAP to print fibonacci series upto n terms.
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a=0,b=1,c,n,i;
+	printf("Enter Number of Terms");
+	scanf("%d",&n);
+	for(i=1;i=n;i++)
+	{
+		c=a+b;
+		printf("%d\n",a);
+		a=b;
+		b=c;
+	}
+	return(0);
+}
